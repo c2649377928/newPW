375d09f537804b21441c86726b75b9ac80cd718d
Chenxiangyu <2649377928@qq.com>
2025-10-13 14:32:51 +0800

    补充类型声明

---
+++ src/components.d.ts
@@ -0,0 +1,17 @@
+declare module './components/ThemeToggle.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+declare module './components/ScrollProgress.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+declare module '../views/HomeView.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
